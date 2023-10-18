0a300f069d58180fc60bf591c446a691354c2d6c
Patrick-Tomic <pat12tomic@gmail.com>
2023-10-18 05:03:09 -0400

    updated webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-alert("hello world");
+import _ from 'lodash';
+import './styles/main.scss';
+import Logo from './flag.png'
+
+function build(){
+    const elements = document.createElement('div');
+   const head = document.createElement('h2');
+   const img = document.createElement('img');
+   head.innerHTML = "Pat's Bosnian American fusion";
+   elements.appendChild(head);
+    img.setAttribute('id','logo');
+    img.src = Logo;
+    
+    elements.appendChild(img);
+    return elements;
+}
+const content = document.querySelector('#content');
+content.appendChild(build());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+    mode: 'development',
+  entry:{ bundle:path.resolve(__dirname, 'src/index.js'),
+},
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean:true,
   },
-};
+  module:{
+    rules:[
+        {
+            test:/\.scss$/,
+            use:['style-loader','css-loader','sass-loader'],
+        },
+        {
+            test:/\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+    ],
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        title:'Restaurant App',
+        filename:'index.html',
+        template: 'src/template.html',
+        
+    })
+  ]
+}
